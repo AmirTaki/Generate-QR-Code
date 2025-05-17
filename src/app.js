a6edb2e94349a580a4dbfc59cc34b8782c8c6dee
amir <amirtaki623@gmail.com>
2025-05-17 21:06:49 +0330

    js : query Selector

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const downloadBtn = document.getElementById("downloadBtn")
+const  generateBtn = document.getElementById("generateBtn")
+const  qrtText = document.getElementById("qr-text")
+const  sizes = document.getElementById("sizes")
+
+const qrContainer = document.querySelector('.qr-body')
