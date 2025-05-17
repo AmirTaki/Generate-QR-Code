3fd550f234e2501fc1283f2c396825b416b50d2f
amir <amirtaki623@gmail.com>
2025-05-17 21:17:23 +0330

    js : generateQRcode & addeventListener

--- src/app.js
+++ src/app.js
@@ -4,3 +4,18 @@ const  qrtText = document.getElementById("qr-text")
 const  sizes = document.getElementById("sizes")
 
 const qrContainer = document.querySelector('.qr-body')
+
+const generateQRCode = () => {
+    new QRCode (qrContainer , {
+        text : qrtText.value,
+        width: size,
+        height: size,
+        colorDark : "#000000",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    })
+}
+generateBtn.addEventListener('click', (eve)=> {
+    eve.preventDefault();
+    generateQRCode()
+})
